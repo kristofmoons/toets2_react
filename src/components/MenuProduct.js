9c4d1a8e68ac1423cf09d3ecb87d39efa9018de9
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-04-15 12:38:07 +0200

    styling css-in-js

--- src/components/MenuProduct.js
+++ src/components/MenuProduct.js
@@ -2,10 +2,14 @@ export function MenuProduct(props) {
     const {product} = props;
     if (!product?.name) return;
 
-    const size = product.size && `(${product.size}cl)`;
+    const size = product.size && ` (${product.size}cl)`;
     return (
-        <div style={{margin: "2vw", fontSize: "larger"}}>
-            {product.name} {size} -- {product.price} &euro;
+        <div style={{margin: "2vw", fontSize: "larger", display: "flex"}}>
+            <div style={{flex: 1}}>
+                {product.name}
+                <span style={{fontSize: "small", color: "blue"}}>{size}</span>
+            </div>
+            <div style={{flex: 1}}>{product.price} &euro;</div>
         </div>
     );
 }
