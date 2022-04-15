548f01dafedb42f2fb16fbe44f5ba0b45b71faf8
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-04-15 10:09:00 +0200

    first component MenuProduct

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 
+function MenuProduct(props) {
+    const {productName} = props;
+    return <div>{productName}</div>;
+}
+
 function App() {
-  return (
-    <div>
-      <h1>Menu</h1>
-        <div>cola</div>
-        <div>water</div>
-        <div>bier</div>
-        <div>wijn</div>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Menu</h1>
+            <MenuProduct productName="cola"/>
+            <MenuProduct productName="water"/>
+            <MenuProduct productName="bier"/>
+            <MenuProduct productName="wijn"/>
+        </div>
+    );
 }
 
 export default App;
