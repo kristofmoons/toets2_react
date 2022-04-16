d6b7b655581c9c2a8ad2be115c33dcbb9f17202c
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-04-16 10:43:56 +0200

    numbers*2

--- src/pages/NumbersPage.js
+++ src/pages/NumbersPage.js
@@ -7,6 +7,7 @@ export function NumbersPage(props) {
         <div className="mx-3" >
             <Numbers numbers={numbers} title="alle getallen"/>
             <Numbers numbers={numbers.filter(n=>n>6)} title="getallen>6"/>
+            <Numbers numbers={numbers.map(n=>n*6)} title="getallen*2"/>
         </div>
     );
 }
