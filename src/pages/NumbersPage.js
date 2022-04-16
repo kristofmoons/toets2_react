17b22043598dec4ab556a728f68d32ca374f4ecc
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-04-16 10:43:21 +0200

    numbers>6

--- src/pages/NumbersPage.js
+++ src/pages/NumbersPage.js
@@ -6,6 +6,7 @@ export function NumbersPage(props) {
     return (
         <div className="mx-3" >
             <Numbers numbers={numbers} title="alle getallen"/>
+            <Numbers numbers={numbers.filter(n=>n>6)} title="getallen>6"/>
         </div>
     );
 }
