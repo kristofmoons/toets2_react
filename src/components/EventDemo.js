54e74e348933dfa878f32e204f5ee26c49c8bb5c
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-15 17:51:46 +0200

    first react-bootstrap Button

--- src/components/EventDemo.js
+++ src/components/EventDemo.js
@@ -1,4 +1,5 @@
 import {Section} from "./Section";
+import {Button} from "react-bootstrap";
 
 export function EventDemo() {
     return <Section title="events">
@@ -7,5 +8,10 @@ export function EventDemo() {
                     alert("button is clicked");
                 }}>click me please!
         </button>
+        <Button className="m-2"
+                onClick={() => {
+                    alert("bootstrap button is clicked");
+                }}>click me please!
+        </Button>
     </Section>;
 }
