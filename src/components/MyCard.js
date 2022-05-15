69dac37d763dd867856c61ecfc3b744e26876a44
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-15 18:16:44 +0200

    Button in MyCard Component

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -2,7 +2,10 @@ import {Card} from "react-bootstrap";
 
 export function MyCard(props) {
     const {title, children} = props;
-    return <Card className="m-2 p-2 shadow-sm text-center">
+    return <Card className="m-2 p-2 shadow-sm text-center"
+                 onClick={() => {
+                     alert(children);
+                 }}>
         <h5>{title}</h5>
         {children}
     </Card>;
