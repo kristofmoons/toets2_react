d387d312e0ef69270dd209efa9b44216d357d0d5
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-15 18:10:44 +0200

    Button in section title

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,16 @@
-import {Container, Row} from "react-bootstrap";
+import {Button, Container, Row} from "react-bootstrap";
 import PropTypes from "prop-types";
 
 export function Section(props) {
     const {title, children} = props;
     return (
         <div className="mt-3 rounded shadow-sm" style={{backgroundColor: "lavender"}}>
-            <h2 className="text-center">{title}</h2>
+            <h2 className="text-center">{title}
+                <Button variant="outline-primary" size="sm" className="ms-2 mt-1"
+                        onClick={() => {
+                            alert("klik klik klik");
+                        }}>klik</Button>
+            </h2>
             <Container>
                 <Row>
                     {children}
