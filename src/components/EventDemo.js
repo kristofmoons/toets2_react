ca9131c54832751a98de671028b6f4516c860474
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-15 18:30:15 +0200

    link and button (preventDefault and stopPropagation)

--- src/components/EventDemo.js
+++ src/components/EventDemo.js
@@ -13,5 +13,25 @@ export function EventDemo() {
                     alert(`bootstrap button is clicked - pos: (${e.clientX}, ${e.clientY})`);
                 }}>click me please!
         </Button>
+        <div style={{backgroundColor: "pink"}}
+             onClick={() => {
+                 alert("DIV is clicked");
+             }}>
+            <p>Dit is een div. Op deze div zit een event handler.</p>
+            <a href="http://google.com"
+               onClick={(e) => {
+                   e.preventDefault();
+                   e.stopPropagation();
+                   alert("link is clicked");
+               }}>dit is een link met een href</a>
+            &nbsp;- en -&nbsp;
+            <Button
+                onClick={(e) => {
+                    e.stopPropagation();
+                    alert("button is clicked");
+                }}>
+                dit is een button
+            </Button>
+        </div>
     </Section>;
 }
