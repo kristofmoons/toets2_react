c5fc2618f96b4332fd644e18fd61ea76aff482e2
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-15 18:18:50 +0200

    show title if it is given (better then complete children)

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -4,7 +4,7 @@ export function MyCard(props) {
     const {title, children} = props;
     return <Card className="m-2 p-2 shadow-sm text-center"
                  onClick={() => {
-                     alert(children);
+                     alert(title || children);
                  }}>
         <h5>{title}</h5>
         {children}
