eea26b1c6267731df0ae13540d817ea84693ae26
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-16 14:24:09 +0200

    styling

--- src/components/OnOffDemo.js
+++ src/components/OnOffDemo.js
@@ -5,12 +5,12 @@ export function OnOffDemo() {
     const [isOn, setIsOn] = useState(false);
 
     return (
-        <>
+        <div className="my-3 py-2 border border-secondary">
             <div>huidige waarde van isOn is: {isOn ? "ON" : "OFF"}</div>
             <div>
                 <MyButton onClick={() => setIsOn(false)}>off</MyButton>
                 <MyButton onClick={() => setIsOn(true)}>on</MyButton>
             </div>
-        </>
+        </div>
     );
 }
