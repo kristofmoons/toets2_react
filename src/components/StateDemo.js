3aca7ddc5b6e4f2e7cdddf52b787898a37eda58d
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-16 13:11:29 +0200

    StateDemo: demoValue with several buttons

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -9,7 +9,11 @@ export function StateDemo() {
         <div>de waarde van demoValue is {demoValue}</div>
         <div>
             <Button variant="primary" size="sm" className="ms-2 mt-1"
+                    onClick={() => setDemoValue(0)}> SET 0 </Button>
+            <Button variant="primary" size="sm" className="ms-2 mt-1"
                     onClick={() => setDemoValue(77)}> SET 77 </Button>
+            <Button variant="primary" size="sm" className="ms-2 mt-1"
+                    onClick={() => setDemoValue(184)}> SET 184 </Button>
         </div>
     </Section>;
 }
