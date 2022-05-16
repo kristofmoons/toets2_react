4b325cc705f0f10fae0b449a7dbfd4430fd9b559
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-16 15:10:02 +0200

    DemoValueReadOnly: lift state up

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -4,28 +4,40 @@ import {MyButton} from "./MyButton";
 import {Counter} from "./Counter";
 import {OnOffDemo} from "./OnOffDemo";
 
-function DemoValue() {
-    const [demoValue, setDemoValue] = useState(0);
+function DemoValue(props) {
+    const {demoValue, onDemoValueChange} = props;
+
     return (
         <div className="my-3 py-2 border border-secondary">
             <div>de waarde van demoValue is {demoValue}</div>
             <div>de waarde van demoValue is {demoValue}</div>
             <div>
-                <MyButton onClick={() => setDemoValue(0)}> SET 0 </MyButton>
-                <MyButton onClick={() => setDemoValue(77)}> SET 77 </MyButton>
-                <MyButton onClick={() => setDemoValue(184)}> SET 184 </MyButton>
+                <MyButton onClick={() => onDemoValueChange(0)}> SET 0 </MyButton>
+                <MyButton onClick={() => onDemoValueChange(77)}> SET 77 </MyButton>
+                <MyButton onClick={() => onDemoValueChange(184)}> SET 184 </MyButton>
             </div>
         </div>
     );
 }
 
+function DemoValueReadOnly(props) {
+    const {demoValue} = props;
+    return (
+        <div className="my-3 py-2 border border-info">
+            <div>demoValue={demoValue}</div>
+        </div>
+    );
+}
+
 export function StateDemo() {
+    const [demoValue, setDemoValue] = useState(0);
 
     return <Section title="state">
-        <DemoValue/>
+        <DemoValue demoValue={demoValue} onDemoValueChange={setDemoValue}/>
         <Counter name="counter1"/>
         <Counter name="counter2"/>
         <OnOffDemo/>
+        <DemoValueReadOnly demoValue={demoValue}/>
     </Section>;
 }
 
