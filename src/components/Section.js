8c772eaf573b5e56932663d9666138a2327bef8b
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-16 15:14:52 +0200

    Section with isOpen (step1)

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,8 +1,22 @@
 import {Button, Container, Row} from "react-bootstrap";
 import PropTypes from "prop-types";
+import {useState} from "react";
+
+function SectionContent(props) {
+    const {isOpen, children} = props;
+    if (!isOpen) return;
+    return (
+        <Container>
+            <Row>
+                {children}
+            </Row>
+        </Container>
+    );
+}
 
 export function Section(props) {
     const {title, children} = props;
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="my-3 p-1 rounded shadow-sm" style={{backgroundColor: "lavender"}}>
             <h2 className="text-center">{title}
@@ -11,11 +25,7 @@ export function Section(props) {
                             alert("klik klik klik");
                         }}>klik</Button>
             </h2>
-            <Container>
-                <Row>
-                    {children}
-                </Row>
-            </Container>
+            <SectionContent isOpen={isOpen}>{children}</SectionContent>
         </div>
     );
 }
