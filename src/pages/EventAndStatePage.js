a19f4b5c22406809fd9b8ea33a5ce26ab757f49c
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-16 13:09:40 +0200

    StateDemo: demoValue

--- src/pages/EventAndStatePage.js
+++ src/pages/EventAndStatePage.js
@@ -1,9 +1,11 @@
 import {EventDemo} from "../components/EventDemo";
+import {StateDemo} from "../components/StateDemo";
 
 export function EventAndStatePage() {
     return (
         <div>
             <EventDemo/>
+            <StateDemo/>
         </div>
     );
 }
