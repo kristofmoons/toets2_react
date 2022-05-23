a4d3fe3c958d5e27defc32cd25d4cc6eec010178
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-05-23 14:43:01 +0200

    favorite numbers: make number array unique

--- src/pages/FavoriteNumberPage.js
+++ src/pages/FavoriteNumberPage.js
@@ -6,7 +6,7 @@ export function FavoriteNumberPage(props) {
     const [favorite, setFavorite] = useState();
     return (
         <div className="mx-3">
-            <Numbers numbers={numbers} title="alle getallen"
+            <Numbers numbers={[...new Set(numbers)]} title="alle getallen"
                      isInitiallyOpen={true}
                      markedNumber={favorite}
                      onSelectNumber={n => setFavorite(n)}/>
