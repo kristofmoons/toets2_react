f7fc2be1e17d71c5c19ae24e4e76436ac5c0228f
kristofmoons <r0696321@student.thomasmore.be>
2022-10-21 11:45:57 +0200

    Vraag 1

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,14 +1,46 @@
 import {Section} from "./Section";
-import {Col} from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
 import {MyCard} from "./MyCard";
+
+function DrinkNote(props) {
+    const {drink} = props;
+    if (!drink.note) return;
+
+    return (
+        <Row className="fs-6">
+            <Col>
+               opm: {drink.note}
+            </Col>
+        </Row>
+    );
+}
+
+function DrinkType(props) {
+    const {drink} = props;
+    if (!drink.warm !==true) return  <Row className="fs-6">
+        <Col>
+            type: Warm
+        </Col>
+    </Row>;
+
+    return ( <Row className="fs-6">
+            <Col>
+                type: Koud
+            </Col>
+        </Row>
+
+    );
+}
+
 
 function Drink(props) {
     const {drink} = props;
     return (
         <Col xs={6} sm={4} md={3} lg={2}>
             <MyCard title={drink.name}>
-                <div>{drink.price}</div>
-                <div>{drink.type}</div>
+                <div>prijs: {drink.price}</div>
+                <DrinkNote drink={drink}/>
+                <DrinkType drink={drink}/>
             </MyCard>
         </Col>
     );
