ad172a8848c3951c52462ccbfed5e958c6e7798e
kristofmoons <r0696321@student.thomasmore.be>
2022-10-21 12:20:03 +0200

    vraag 2 ! buttons werken maar kaart word nog niet aangepast

--- src/pages/DrinksPage.js
+++ src/pages/DrinksPage.js
@@ -1,13 +1,30 @@
 import {Drinks} from "../components/Drinks";
+import {Form} from "react-bootstrap";
+import {useState} from "react";
 
 
 export function DrinksPage(props) {
     const {drinks} = props;
-    console.log(drinks)
+    const [warm, setWarm] = useState(0);
     return (
         <div className="mx-3">
+            <div className="m-3">
+                <Form className="mx-3">
+                    <Form.Label>Warme dranken?</Form.Label><br/>
+                    <Form.Check inline checked={warm ===0} label="geen voorkeur" name="warm"
+                                type="radio" id="warm-1" onChange={() => setWarm(0)}/>
+                    <Form.Check inline checked={warm === 1} label="neen" name="warm"
+                                type="radio" id="warm-2" onChange={() => setWarm(1)}/>
+                    <Form.Check inline checked={warm ===2} label="ja" name="warm"
+                                type="radio" id="warm-3" onChange={() => setWarm(2)}/>
+                </Form>
+            </div>
 
             <Drinks drinks={drinks} title="DrankenKaart" isInitiallyOpen={true}/>
+
+        }
+
+
 
         </div>
     );
